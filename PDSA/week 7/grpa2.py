90f764ac921288e6696a41f3b69b74c88db3129b
Ishant Jindal <83600760+ij007@users.noreply.github.com>
2022-02-13 18:11:13 +0530

    Update grpa2.py

---
+++ PDSA/week 7/grpa2.py
@@ -0,0 +1,27 @@
+def minimum_platform(train_schedule):
+    count = 1
+    train_list = []
+    for (i,j,k) in train_schedule:
+        train_list.append((int(j.replace(':','')),int(k.replace(':','')),i))
+    train_list.sort()
+    train_at_plateform = []
+    for train in train_list:        
+        
+        t = len(train_at_plateform)-1
+        while t >= 0:
+            if train[0] > train_at_plateform[t][1]:
+                train_at_plateform.pop(t)
+            t = t-1        
+        t = len(train_at_plateform)-1
+        while t >= 0:
+            if train[0] < train_at_plateform[t][1]:
+                t = t - 1        
+            elif train[1] > train_at_plateform[t][1]:
+                train_at_plateform.pop(t)
+                t = t - 1                
+        train_at_plateform.append(train)                    
+        if len(train_at_plateform) > count:
+            count = len(train_at_plateform)
+    return count
+schedule = eval(input())           
+print(minimum_platform(schedule))
