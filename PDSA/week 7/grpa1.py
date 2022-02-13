24fdf311c49fc7212adecf81ff10dd33bb497194
Ishant Jindal <83600760+ij007@users.noreply.github.com>
2022-02-13 16:41:10 +0530

    Update grpa1.py

--- PDSA/week 7/grpa1.py
+++ PDSA/week 7/grpa1.py
@@ -1,26 +1,120 @@
-def minbin(num,init):
-  import math
-  mini = math.inf
-  min_bin = 0
-  for key in init:
-    if (num + init[key]) < mini:
-      min_bin = key
-      mini = (num + init[key])
-  return min_bin
+class AVLTree:
+    # Constructor:
+    def __init__(self,initval=None):
+        self.value = initval
+        if self.value:
+            self.left = AVLTree()
+            self.right = AVLTree()
+            self.height = 1
+        else:
+            self.left = None
+            self.right = None
+            self.height = 0
+        return
 
-def minimizeLateness(n,jobs):
-  newj = []
-  for (h,u,v) in jobs:
-    newj.append((v,u,h))
-  newj.sort()
+    def isempty(self):
+        return (self.value == None)
 
-  init = {}
-  for i in range(n):
-    init[i] = 0
-  bins  = [[] for x in range(n)]
+    def isleaf(self):
+        return (self.value != None and self.left.isempty() and self.right.isempty())
 
-  for job in newj:
-    min_bin = minbin(job[1], init)
-    bins[min_bin].append(job[2])
-    init[min_bin] = init[min_bin] + job[1]
-  return bins
+    def leftrotate(self):
+        v = self.value
+        vr = self.right.value
+        tl = self.left
+        trl = self.right.left
+        trr = self.right.right
+        newleft = AVLTree(v)
+        newleft.left = tl
+        newleft.right = trl
+        self.value = vr
+        self.right = trr
+        self.left = newleft
+        return
+    def rightrotate(self):
+        v = self.value
+        vl = self.left.value
+        tll = self.left.left
+        tlr = self.left.right
+        tr = self.right
+        newright = AVLTree(v)
+        newright.left = tlr
+        newright.right = tr
+        self.right = newright
+        self.value = vl
+        self.left = tll
+        return
+    def insert(self,v):
+        if self.isempty():
+            self.value = v
+            self.left = AVLTree()
+            self.right = AVLTree()
+            self.height = 1
+            return        
+        if self.value == v:
+            return        
+        if v < self.value:
+            self.left.insert(v)
+            self.rebalance()
+            self.height = 1 + max(self.left.height, self.right.height)            
+        if v > self.value:
+            self.right.insert(v)
+            self.rebalance()            
+            self.height = 1 + max(self.left.height, self.right.height)    
+                              
+    def rebalance(self):
+        if self.left == None:
+            hl = 0
+        else:
+            hl = self.left.height
+        if self.right == None:
+            hr = 0
+        else:
+            hr = self.right.height                        
+        if  hl - hr > 1:
+            if self.left.left.height > self.left.right.height:
+                self.rightrotate()
+            if self.left.left.height < self.left.right.height:
+                self.left.leftrotate()
+                self.rightrotate()
+            self.updateheight()        
+        if  hl - hr < -1:
+            if self.right.left.height < self.right.right.height:
+                self.leftrotate()
+            if self.right.left.height > self.left.right.height:
+                self.right.rightrotate()
+                self.leftrotate()
+            self.updateheight()
+            
+    def updateheight(self):
+        if self.isempty():
+            return
+        else:
+            self.left.updateheight()
+            self.right.updateheight()
+            self.height = 1 + max(self.left.height, self.right.height)
+        
+    def inorder(self):
+        if self.isempty():
+            return([])
+        else:
+            return(self.left.inorder()+ [self.value]+ self.right.inorder())
+    def preorder(self):
+        if self.isempty():
+            return([])
+        else:
+            return([self.value] + self.left.preorder()+  self.right.preorder())
+    def postorder(self):
+        if self.isempty():
+            return([])
+        else:
+            return(self.left.postorder()+ self.right.postorder() + [self.value])
+
+A = AVLTree()
+nodes = eval(input())
+for i in nodes:
+    A.insert(i)
+
+print(A.inorder())
+print(A.preorder())
+print(A.postorder())
